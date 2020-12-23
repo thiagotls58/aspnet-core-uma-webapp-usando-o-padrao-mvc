b0e3119f22553f10d06dadbc3e9f76c25e9e505d
Thiago Luz <thiagotls58@gmail.com>
2020-12-23 08:38:43 -0300

    Aula 02

---
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app) 
+        {
+            // IApplicationBuilder é resposável pela configuração do Pipeline da requisição
+
+            app.Run(Roteamento);
+
+        }
+
+        public Task Roteamento(HttpContext contexto)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var caminhosAtendidos = new Dictionary<string, RequestDelegate>
+            {
+                { "/Livros/ParaLer", LivrosParaLer },
+                { "/Livros/Lendo", LivrosLendo },
+                { "/Livros/Lidos", LivrosLidos }
+            };
+
+            if (caminhosAtendidos.ContainsKey(contexto.Request.Path))
+            {
+                var metodo = caminhosAtendidos[contexto.Request.Path];
+                return metodo.Invoke(contexto);
+            }
+
+            contexto.Response.StatusCode = 404;
+            return contexto.Response.WriteAsync("Caminho inexistente.");
+        }
+
+        public Task LivrosParaLer(HttpContext contexto)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            return contexto.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
+
+        public Task LivrosLendo(HttpContext contexto)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            return contexto.Response.WriteAsync(_repo.Lendo.ToString());
+        }
+
+        public Task LivrosLidos(HttpContext contexto)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            return contexto.Response.WriteAsync(_repo.Lidos.ToString());
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# aspnet-core-uma-webapp-usando-o-padrao-mvc
- Asp.NET Core: Uma webapp usando o padrão MVC
+# Asp.NET Core: Uma webapp usando o padrão MVC
+
+## Aulas
+1. Transformando a aplicação em um servidor web
+2. Atendendo requisições diferentes
+3. Usando o serviço de roteamento do AspNet Core
+4. Melhorando a experiência do usuário com HTML
+5. Organizando a aplicação
+6. Introduzindo o AspNet Core MVC
+7. Renderização de views no AspNet Core MVC
+
